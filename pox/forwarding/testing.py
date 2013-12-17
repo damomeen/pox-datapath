f2aab7714633d9aaa2556e7b7c6c7866034a6d6e
damomeen <damianp@man.poznan.pl>
2013-12-17 10:14:24 -0500

    test modifications

--- pox/forwarding/testing.py
+++ pox/forwarding/testing.py
@@ -1,11 +1,12 @@
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.util import dpidToStr
+from pox.lib.addresses import EthAddr
 
 log = core.getLogger()
 
 
-def __addFlowmod (event):
+def __addFlowmod1 (event):
   msg = of.ofp_flow_mod()
   msg.actions.append(of.ofp_action_output(port = 1))
   try:
@@ -14,6 +15,21 @@ def __addFlowmod (event):
   event.connection.send(msg)
   log.info("Redirecting all traffic to port 1 %s", dpidToStr(event.dpid))
 
+def __addFlowmod2 (event):
+  msg = of.ofp_flow_mod()
+  msg.match.dl_dst = EthAddr("35:35:35:35:35:35")
+  msg.match.dl_src = EthAddr("45:45:45:45:45:45")
+  msg.match.dl_vlan = 1234
+  msg.match.dl_vlan_pcp = 1
+  msg.match.dl_type = 0x0806
+  msg.actions.append(of.ofp_action_output(port = 1))
+  try:
+    msg.buffer_id = event.ofp.buffer_id
+  except: pass
+  event.connection.send(msg)
+  log.info("Redirecting all traffic to port 1 %s", dpidToStr(event.dpid))
+
+  
 def __redirectPacket (event):
   msg = of.ofp_packet_out()
   msg.data = event.ofp
@@ -28,7 +44,7 @@ def _handle_ConnectionUp (event):
   msg = of.ofp_stats_request(body=of.ofp_port_stats_request())
   #event.connection.send(msg)
   #log.info("Port stats request sent")
-  __addFlowmod(event)
+  __addFlowmod2(event)
 
 
 def _handle_PacketIn (event):
@@ -42,6 +58,6 @@ def _handle_PortStats (event):
 
 def launch ():
   core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
-  core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
+  #core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
   #core.openflow.addListenerByName("PortStatsReceived",  _handle_PortStats)
   log.info("Testing controller running.")
